948b59f1187b12b56fd049a621904e26f023bff9
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-06-22 03:40:04 +0200

    added GameOver component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
 const initialGameBoard = [
@@ -46,6 +47,9 @@ winner = firstSquareSymbol
   }
 }
 
+
+const hasDraw = gameTurns.length === 9 && !winner
+
   function handleSelectSquare(rowIndex, colIndex) {
     //setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
@@ -73,7 +77,7 @@ winner = firstSquareSymbol
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}/>
