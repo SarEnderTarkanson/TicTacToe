d3933c37e4f1657da4baf9b0d91f8e9d7b62db09
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-25 02:17:48 +0100

    added the configuration for the PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/TicTacToe/"
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Tic Tac Toe Game",
+        short_name: "TicTacToe",
+        description: "A fun 2-player tic tac toe game",
+        theme_color: "#fcd256",
+        background_color: "#282617",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/TicTacToe/",
+});
